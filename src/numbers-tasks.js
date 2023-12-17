140b7808bc1823571646f4cef9d372d7123ec0c1
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-12-17 23:48:47 +0300

    feat: add tasks 27, 28

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -462,8 +462,8 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /** 28
@@ -477,8 +477,8 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
 }
 
 /** 29
